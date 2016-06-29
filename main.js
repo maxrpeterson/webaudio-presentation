b59873f3642a9ff311620c2c9766131dd5a006fd
Max Peterson <petersonmaxr@gmail.com>
2016-06-29 00:37:57 -0400

    adds QwertyHancock

--- main.js
+++ main.js
@@ -3,12 +3,43 @@ const audioCtx = new AudioContext();
 
 // create the oscillator & change some settings for better tone
 const osc1 = audioCtx.createOscillator();
+// set the waveform
 osc1.type = 'sawtooth';
 // `noteNameToMidiNumber` & `noteNameToFrequency` imported from helper js file
 osc1.frequency.value = noteNameToFrequency('C4'); // C4 is middle C
 
-// create the gain node
-const gain = audioCtx.createGain();
+// create the amp (gain node)
+const amp = audioCtx.createGain();
+amp.gain.value = 0;
 
 // link them up
-osc1.connect(gain).connect(audioCtx.destination);
+osc1.connect(amp).connect(audioCtx.destination);
+
+// start the oscillator
+osc1.start();
+
+// set up the QwertyHancock keyboard
+var keyboard = new QwertyHancock({
+       id: 'keyboard',
+       width: 600,
+       height: 150,
+       octaves: 2,
+       startNote: 'A3',
+       whiteNotesColour: 'white',
+       blackNotesColour: 'black',
+       hoverColour: '#f3e939'
+    });
+
+// add the keyDown listener
+keyboard.keyDown = function(note) {
+  console.log(note, noteNameToFrequency(note));
+  osc1.frequency.setValueAtTime(noteNameToFrequency(note), audioCtx.currentTime);
+  amp.gain.cancelScheduledValues(audioCtx.currentTime);
+  amp.gain.exponentialRampToValueAtTime(1, audioCtx.currentTime + 0.1);
+};
+
+// add the keyUp listener
+keyboard.keyUp = function(note) {
+  amp.gain.cancelScheduledValues(audioCtx.currentTime);
+  amp.gain.exponentialRampToValueAtTime(0.0001, audioCtx.currentTime + 2);
+};
